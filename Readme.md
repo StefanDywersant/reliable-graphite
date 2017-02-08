98557b264d1cf5df9652e9fbca34ea2298ad37f5
Karol Maciaszek <karol.maciaszek@contractors.roche.com>
2017-02-08 15:20:42 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,65 @@
+# reliable-graphite
+
+Graphite client which buffers metrics on remote service downtime.
+
+## Installation
+
+	$ npm install --save reliable-graphite
+
+## Example
+
+### Simple usage
+
+	const Graphite = require('reliable-graphite'),
+		graphite = new Graphite(<host>, <port>);
+
+	graphite.push('metric1', 100);
+	
+This will produce logs similar to:
+
+	info: HTTP karol@127.0.0.1 GET / took=11ms
+
+## API
+
+	const graphite = new Graphite(host, port, {
+		socket_timeout: 300000,
+		socket_reconnect_delay: 1000,
+		queue_size_limit: 10000000,
+		chunk_size: 200
+	});
+
+Where config is an object with following optional properties:
+
+* socket_timeout - *number (default 300000)* - Graphite socket inactivity time (in milliseconds) after which it will be disconnected
+
+* socket_reconnect_delay - *number (default 1000)* - Time (in milliseconds) to wait before reconnecting socket. This prevents falling into reconnect loop at high speed when e.g. graphite server is down.
+
+* queue_size_limit - *number (default 10000000)* - When Graphite server is down the metric lines will be buffered up to this count. *graphite.push()* will throw exception once this limit is exceeded.
+
+* chunk_size - *number (default 200)* - Indicates how many metric lines will be pushed to Graphite server at once
+
+
+	graphite.push(name, value, ts);
+	
+Arguments:
+
+* name - *string* - Metric name
+
+* value - *number* - Metric value
+
+* ts - *number (default Date.now())* - Metric timestamp (in milliseconds)
+
+
+Karol Maciaszek <karol@maciaszek.pl>
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2016 Karol Maciaszek <karol@maciaszek.pl>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ graphite.js
@@ -0,0 +1,142 @@
+'use strict';
+
+
+const os = require('os'),
+	Socket = require('net').Socket;
+
+
+/**
+ * Provides interface for pushing metric lines to Graphite service.
+ */
+class Graphite {
+
+
+	/**
+	 * Initializes Graphite service.
+	 */
+	constructor(host, port, config) {
+		this.config = Object.assign({
+			socket_timeout: 300000,
+			socket_reconnect_delay: 1000,
+			queue_size_limit: 10000000,
+			chunk_size: 200,
+			logger: (severity, message) => console[severity](message)
+		}, config);
+
+		this.port = port;
+		this.host = host;
+
+		this.queue = [];
+		this._socket = null;
+	}
+
+
+	/**
+	 * Returns initialized socket connected to Graphite service.
+	 *
+	 * @returns {Promise<Socket>}
+	 */
+	get socket() {
+		if (this._socket) {
+			return Promise.resolve(this._socket);
+		}
+
+		return new Promise((resolve, reject) => {
+			const socket = new Socket({
+				writable: true,
+				allowHalfOpen: false
+			});
+
+			socket.setTimeout(this.config.socket_timeout);
+
+			socket
+				.on('connect', () => resolve(this._socket = socket))
+				.on('error', (error) => this.config.logger('error', error.stack))
+				.on('timeout', () => {
+					this.config.logger('warn', 'Socket timeout out');
+					socket.end();
+				})
+				.on('close', () => {
+					this._socket = null;
+					reject(new Error());
+				});
+
+			socket.connect(this.port, this.host);
+		});
+	}
+
+
+	/**
+	 * Sends metric lines to Graphite service.
+	 *
+	 * @param {string[]} lines Metric lines
+	 * @returns {Promise}
+	 */
+	send(lines) {
+		return this.socket.then((socket) => {
+			return lines.reduce((p, line) =>
+					p.then(() =>
+						new Promise(resolve => socket.write(line, () => resolve()))
+					),
+				Promise.resolve()
+			);
+		});
+	}
+
+
+	/**
+	 * Starts processing of the queued metric lines.
+	 */
+	run() {
+		// give up if the queue is already processed
+		if (this.running) {
+			return;
+		}
+
+		// give up if there is nothing to process
+		if (!this.queue.length) {
+			return;
+		}
+
+		// process queue
+		this.running = true;
+
+		const messages = this.queue.slice(0, this.config.chunk_size);
+
+		this.send(messages)
+			.then(() => this.queue.splice(0, messages.length))
+			.catch(error => {
+				this.config.logger('error', error.stack);
+				return new Promise(resolve => setTimeout(resolve, this.config.socket_reconnect_delay));
+			})
+			.then(() => {
+				this.config.logger('log', `Sent ${messages.length} line(s), ${this.queue.length} still in queue`);
+				this.running = false;
+				this.run();
+			});
+	}
+
+	/**
+	 * Sends metric line to Graphite service.
+	 *
+	 * @param {string} name Metric name
+	 * @param {number} value Metric value
+	 * @param {string} ts Metric timestamp
+	 */
+	push(name, value, ts) {
+		if (this.queue.length > this.config.queue_size_limit) {
+			throw new Error('Too many metric lines in send queue, not accepting more');
+		}
+
+		ts = ts || Date.now();
+
+		this.queue.push(`${name} ${value} ${Math.round(ts / 1000)}${os.EOL}`);
+
+		// start processing in next event loop iteration
+		// this will allow to gather more metric lines
+		process.nextTick(() => this.run());
+	}
+
+}
+
+module.exports = Graphite;
